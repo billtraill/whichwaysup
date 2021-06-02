c8e1a2c2e18c5ae0b5766d46fc4ddf059157f4ee
Bill Traill <bill.traill@gmail.com>
2021-06-02 11:55:41 +0100

    Used game quaterione and added shake

--- imu_webservice.py
+++ imu_webservice.py
@@ -4,11 +4,14 @@ import web
 import time
 import board
 import busio
+import adafruit_bno08x
 from adafruit_bno08x import (
     BNO_REPORT_ACCELEROMETER,
     BNO_REPORT_GYROSCOPE,
     BNO_REPORT_MAGNETOMETER,
     BNO_REPORT_ROTATION_VECTOR,
+    BNO_REPORT_GAME_ROTATION_VECTOR,
+    BNO_REPORT_SHAKE_DETECTOR
 )
 from adafruit_bno08x.i2c import BNO08X_I2C
 
@@ -18,12 +21,16 @@ bno = BNO08X_I2C(i2c)
 bno.enable_feature(BNO_REPORT_ACCELEROMETER)
 bno.enable_feature(BNO_REPORT_GYROSCOPE)
 bno.enable_feature(BNO_REPORT_MAGNETOMETER)
-bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+# bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+
+bno.enable_feature(BNO_REPORT_GAME_ROTATION_VECTOR)
+bno.enable_feature(adafruit_bno08x.BNO_REPORT_SHAKE_DETECTOR)
 
 urls = (
     '/(.*)', 'hello'
 )
 app = web.application(urls, globals())
+
 
 class hello:
     def GET(self, name):
@@ -31,17 +38,32 @@ class hello:
             name = 'World'
 #        return 'Hello, ' + name + '!'
         accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
-        acceleration = '"acceleration":[%0.6f,%0.6f,%0.6f]'% (accel_x, accel_y, accel_z)
+        acceleration = '"accel":[%0.6f,%0.6f,%0.6f]' % (
+            accel_x, accel_y, accel_z)
         gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
-        gyro  = '"gyro":[%0.6f,%0.6f,%0.6f]'% (gyro_x, gyro_y, gyro_z)
+        gyro = '"gyro":[%0.6f,%0.6f,%0.6f]' % (gyro_x, gyro_y, gyro_z)
         mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
-        magnetometer  = '"magnetometer":[%0.6f,%0.6f,%0.6f]'% (mag_x, mag_y, mag_z)
-        quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
-        quaternione  = '"quaternione":[%0.6f,%0.6f,%0.6f]'% (quat_i, quat_j, quat_k)
-        quaternione_real  = '"quaternione_real":%0.6f'%  quat_real
+        magnetometer = '"mag":[%0.6f,%0.6f,%0.6f]' % (
+            mag_x, mag_y, mag_z)
+        # quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
+        # quaternione = '"quat":[%0.6f,%0.6f,%0.6f,%0.6f]' % (
+        #    quat_i, quat_j, quat_k, quat_real)
 
-        return '{%s,%s,%s,%s,%s}' % (acceleration,gyro,magnetometer,quaternione,quaternione_real)
-        #return '{"quaternione":[%f,%f,%f]}'%(1.0,1.0,1.0)
+        (game_quat_i, game_quat_j, game_quat_k, game_quat_real,
+         ) = bno.game_quaternion  # pylint:disable=no-member
+
+        game_quaternione = '"game_quat":[%0.6f,%0.6f,%0.6f,%0.6f]' % (
+            game_quat_i, game_quat_j, game_quat_k, game_quat_real)
+
+        if bno.shake:
+            shake = '"shake": true'
+        else:
+            shake = '"shake": false'
+
+        print('game_quaternione: %s, shake: %s ' % (game_quaternione, shake))
+        return '{%s,%s,%s,%s,%s}' % (acceleration, gyro, magnetometer, game_quaternione, shake)
+        # return '{"quaternione":[%f,%f,%f]}'%(1.0,1.0,1.0)
+
 
 if __name__ == "__main__":
     app.run()
